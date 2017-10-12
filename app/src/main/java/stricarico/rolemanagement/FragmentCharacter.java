c71c06b5e8fdd2fbe19e1d76b7f5eaa7d7af516a
Santiago Gabriel Tricarico <tsantiagog93@gmail.com>
2017-10-12 01:46:02 -0300

    Armado del diseño

--- app/src/main/java/stricarico/rolemanagement/FragmentCharacter.java
+++ app/src/main/java/stricarico/rolemanagement/FragmentCharacter.java
@@ -1,22 +1,33 @@
 package stricarico.rolemanagement;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class FragmentCharacter extends Fragment {
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_character, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_character, container, false);
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.newCharacter);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent resultIntent = new Intent(getActivity(), CharacterActivity.class);
+                startActivity(resultIntent);
+            }
+        });
+
+        return view;
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
 }
